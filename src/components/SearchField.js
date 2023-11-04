47f81140c5897d31733b7c8f342a35fe1b6096d3
soumyabiswas250498 <soumyabiswas250498@gmail.com>
2023-11-04 22:56:59 +0530

    test

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -5,7 +5,7 @@ function SearchField() {
         placeholder="Search a song of your choice"
         className=" w-full h-full px-3 pb-1 rounded-l-xl lato"
       />
-      <div className="w-[66px] h-full flex items-center justify-center bg rounded-r-xl border-l border-black ">
+      <div className="w-[66px] h-full flex items-center justify-center bg rounded-r-xl border-l border-black cursor-pointer ">
         <img src="./Searchicon.svg" alt="search" />
       </div>
     </div>
